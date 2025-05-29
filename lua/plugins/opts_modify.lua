412ba48ea51fa5fa36c0134086cc92627f6801fa
Vikrant Shah <60457380+vikrrantshah@users.noreply.github.com>
2025-05-29 14:35:27 +0200

    Show hidden files by default

--- lua/plugins/opts_modify.lua
+++ lua/plugins/opts_modify.lua
@@ -16,6 +16,16 @@ return {
   {
     "snacks.nvim",
     opts = {
+      picker = {
+        sources = {
+          explorer = {
+            -- show hidden files like .env
+            hidden = true,
+            -- show files ignored by git like node_modules
+            ignored = true,
+          },
+        },
+      },
       dashboard = {
         preset = {
           header = [[
